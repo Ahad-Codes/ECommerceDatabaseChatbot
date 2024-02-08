873960af91a36280a34e3bf1ef1222e0a84e3b8d
Azrex03 <24100307@lums.edu.pk>
2024-02-08 20:15:06 +0500

    Updated System Message for tables

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables
+from langchain.schema import SystemMessage
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
+
+
 chat_prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content = f"You are an AI that has access to a SQLite Database. These are the following tables:  \n{tables}"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name='agent_scratchpad')
     ]
