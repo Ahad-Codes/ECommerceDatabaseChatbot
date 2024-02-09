a405cf9ecec54c98688087dba4e523ad5fcb76a1
Azrex03 <24100307@lums.edu.pk>
2024-02-09 20:24:16 +0500

    Added Memory to Agent Executor

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, MessagesPlaceholder
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 from langchain.schema import SystemMessage
 load_dotenv()
 
@@ -16,14 +18,19 @@ chat_prompt = ChatPromptTemplate(
         SystemMessage(content = (
                       "You are an AI that has access to a SQLite Database."
                       f"Here are the tables in the database: {tables}\n"
-                      "Do not make any assumptions about what tables exist or what columns they have. You can ask use the describe tables function."
+                      "Do not make any assumptions about what tables exist or what columns they have. You can use the describe tables function."
         )),
+        MessagesPlaceholder(variable_name= 'chat_history'),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name='agent_scratchpad')
     ]
 )
 
-tools = [run_query_tool, describe_tables_tool]
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+
+
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(
     llm = chat,
@@ -34,7 +41,9 @@ agent = OpenAIFunctionsAgent(
 agent_exec = AgentExecutor(
     agent= agent,
     verbose= True,
-    tools= tools
+    tools= tools,
+    memory=memory
 )
 
-agent_exec('How many users have provided a shipping address?')
+agent_exec('How many orders are there? Write the report to a file called num_orders.html.')
+agent_exec('Repeat the exact same process for users.')
